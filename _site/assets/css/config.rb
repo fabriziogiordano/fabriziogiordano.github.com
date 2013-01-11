5445a9d6c6b5cc536b203aaeac283e41c96fa70a
Fabrizio Giordano <giordano.fabrizio@gmail.com>
2013-01-11 00:59:18 -0500

    sass and some fonts

--- _site/assets/css/config.rb
+++ _site/assets/css/config.rb
@@ -3,6 +3,6 @@ css_dir = "/"
 sass_dir = "/scss"
 images_dir = "/"
 javascripts_dir = "javascripts"
-output_style = :compact # :expanded or :nested or :compact or :compressed
+output_style = :compressed # :expanded or :nested or :compact or :compressed
 relative_assets = true
 line_comments = false
